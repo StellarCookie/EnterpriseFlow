258d5e79da2dffc5ef31228699dac5a73976edb6
Sandra <romans21@yahoo.com>
2026-06-17 15:27:30 +0300

    modif

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all',
+    allowedHosts: ['sb-5o7sg7ghdnvd.vercel.run'],
     port: 5173,
     proxy: {
       '/api': {
